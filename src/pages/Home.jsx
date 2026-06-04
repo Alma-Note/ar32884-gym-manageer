db583ccfab4f01bc636b9306dd73056f882702f3
Alma-Note <ar32884@seeu.edu.mk>
2026-06-04 13:46:13 +0200

    home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 function Home() {
     return (
         <div>
+            <p>Home</p>
             <h1>Gym Manager</h1>
             <p>Manage your gym members easily.</p>
-            <p>test git commit</p>
+
         </div>
     );
 }
